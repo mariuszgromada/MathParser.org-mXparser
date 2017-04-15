3da0c91a07cfc0909a7352afd5e1e5a439d68ac0
Mariusz Gromada <mariusz.gromada@gmail.com>
2017-04-16 00:06:13 +0200

    Cleaning :-)

--- CURRENT/c-sharp/exe-lib-tests/Run-Tests-Reg/RunTests.cs
+++ CURRENT/c-sharp/exe-lib-tests/Run-Tests-Reg/RunTests.cs
@@ -1,9 +1,13 @@
 ﻿using org.mariuszgromada.math.mxparser.regressiontesting;
+using org.mariuszgromada.math.mxparser;
+using System;
 
 namespace mxparser.runtests {
 	class RunTestsReg {
 		static void Main(string[] args) {
-			RunTest.Start("reg");
-		}
-	}
+			RunTest.Start("syn");
+            mXparser.consolePrintln(".NET CLR version:" + Environment.Version);
+            mXparser.consolePrintln(".NET version:" + System.Diagnostics.FileVersionInfo.GetVersionInfo(typeof(int).Assembly.Location).ProductVersion);
+        }
+    }
 }
