f4130eda1f5407a65aa4971b2764ed87a6c89fb7
Mariusz Gromada <mariusz.gromada@gmail.com>
2017-11-01 17:36:34 +0100

    PerformanceTests - mXparser.disableUlpRounding()

--- CURRENT/java/src/org/mariuszgromada/math/mxparser/regressiontesting/PerformanceTests.java
+++ CURRENT/java/src/org/mariuszgromada/math/mxparser/regressiontesting/PerformanceTests.java
@@ -1,9 +1,9 @@
 /*
- * @(#)PerformanceTests.java       3.0.0    2016-05-07
+ * @(#)PerformanceTests.java       4.2.0    2017-11-01
  *
  * You may use this software under the condition of "Simplified BSD License"
  *
- * Copyright 2010-2016 MARIUSZ GROMADA. All rights reserved.
+ * Copyright 2010-2017 MARIUSZ GROMADA. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are
  * permitted provided that the following conditions are met:
@@ -75,7 +75,7 @@ import org.mariuszgromada.math.mxparser.mXparser;
  *                 <a href="http://sourceforge.net/projects/janetsudoku" target="_blank">Janet Sudoku on SourceForge</a><br>
  *                 <a href="http://bitbucket.org/mariuszgromada/janet-sudoku" target="_blank">Janet Sudoku on BitBucket</a><br>
  *
- * @version        3.0.0
+ * @version        4.2.0
  *
  * @see Expression
  */
