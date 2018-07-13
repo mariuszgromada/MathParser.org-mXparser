0704991a6be66079a5c405200f5c901a224ca690
Mariusz Gromada <mariusz.gromada@gmail.com>
2018-07-13 23:42:08 +0200

    Unit tests wrapper for org.mariuszgromada.math.mxparser.regressiontesting

---
+++ CURRENT/c-sharp/unit-tests/Tests-mXparser-00-Current/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests-mXparser-00-Current")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tests-mXparser-00-Current")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("783f19dc-8ed9-4120-8b7b-bbca8d859d8b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
