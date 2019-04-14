d2e3243c4cfc32b8f085e3f54679f3f45a4a82b1
Apollo3zehn <vwilms@outlook.com>
2019-04-14 18:49:32 +0200

    Clean-up .NET projects.

--- CURRENT/c-sharp/src/org/mariuszgromada/math/mxparser/syntaxchecker/SimpleCharStream.cs
+++ CURRENT/c-sharp/src/org/mariuszgromada/math/mxparser/syntaxchecker/SimpleCharStream.cs
@@ -99,7 +99,7 @@ public class SimpleCharStream{
      try {
         if (stream_closed || (i = inputStream.Read(buffer, maxNextCharInd, available - maxNextCharInd)) <= 0){
            stream_closed = true;
-			#if PCL || CORE || NETSTANDARD
+			#if NETSTANDARD
 				inputStream.Dispose();
 			#else
 				inputStream.Close();
