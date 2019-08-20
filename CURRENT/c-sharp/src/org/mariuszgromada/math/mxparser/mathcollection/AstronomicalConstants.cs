317b3b99e9cd3321c46f658a6d08bce97303e1f2
Adam Spofford <pieflavor.mc@gmail.com>
2019-08-20 08:06:19 -0700

    Document AstronomicalConstants.cs

--- CURRENT/c-sharp/src/org/mariuszgromada/math/mxparser/mathcollection/AstronomicalConstants.cs
+++ CURRENT/c-sharp/src/org/mariuszgromada/math/mxparser/mathcollection/AstronomicalConstants.cs
@@ -56,169 +56,99 @@
 using System;
 
 namespace org.mariuszgromada.math.mxparser.mathcollection {
-	/**
-	 * AstronomicalConstants - class representing the most important astronomical constants.
-	 *
-	 * @author         <b>Mariusz Gromada</b><br>
-	 *                 <a href="mailto:mariuszgromada.org@gmail.com">mariuszgromada.org@gmail.com</a><br>
-	 *                 <a href="http://mathspace.pl" target="_blank">MathSpace.pl</a><br>
-	 *                 <a href="http://mathparser.org" target="_blank">MathParser.org - mXparser project page</a><br>
-	 *                 <a href="http://github.com/mariuszgromada/MathParser.org-mXparser" target="_blank">mXparser on GitHub</a><br>
-	 *                 <a href="http://mxparser.sourceforge.net" target="_blank">mXparser on SourceForge</a><br>
-	 *                 <a href="http://bitbucket.org/mariuszgromada/mxparser" target="_blank">mXparser on Bitbucket</a><br>
-	 *                 <a href="http://mxparser.codeplex.com" target="_blank">mXparser on CodePlex</a><br>
-	 *                 <a href="http://janetsudoku.mariuszgromada.org" target="_blank">Janet Sudoku - project web page</a><br>
-	 *                 <a href="http://github.com/mariuszgromada/Janet-Sudoku" target="_blank">Janet Sudoku on GitHub</a><br>
-	 *                 <a href="http://janetsudoku.codeplex.com" target="_blank">Janet Sudoku on CodePlex</a><br>
-	 *                 <a href="http://sourceforge.net/projects/janetsudoku" target="_blank">Janet Sudoku on SourceForge</a><br>
-	 *                 <a href="http://bitbucket.org/mariuszgromada/janet-sudoku" target="_blank">Janet Sudoku on BitBucket</a><br>
-	 *                 <a href="https://play.google.com/store/apps/details?id=org.mathparser.scalar.lite" target="_blank">Scalar Free</a><br>
-	 *                 <a href="https://play.google.com/store/apps/details?id=org.mathparser.scalar.pro" target="_blank">Scalar Pro</a><br>
-	 *                 <a href="http://scalarmath.org/" target="_blank">ScalarMath.org</a><br>
-	 *
-	 * @version        4.0.0
-	 */
+	/// <summary>AstronomicalConstants - class representing the most important astronomical constants.</summary>
+	///
+	/// <remarks>
+	/// Author: <b>Mariusz Gromada</b><br/>
+	/// <a href="mailto:mariuszgromada.org@gmail.com">mariuszgromada.org@gmail.com</a><br/>
+	/// <a href="http://mathspace.pl" target="_blank">MathSpace.pl</a><br/>
+	/// <a href="http://mathparser.org" target="_blank">MathParser.org - mXparser project page</a><br/>
+	/// <a href="http://github.com/mariuszgromada/MathParser.org-mXparser" target="_blank">mXparser on GitHub</a><br/>
+	/// <a href="http://mxparser.sourceforge.net" target="_blank">mXparser on SourceForge</a><br/>
+	/// <a href="http://bitbucket.org/mariuszgromada/mxparser" target="_blank">mXparser on Bitbucket</a><br/>
+	/// <a href="http://mxparser.codeplex.com" target="_blank">mXparser on CodePlex</a><br/>
+	/// <a href="http://janetsudoku.mariuszgromada.org" target="_blank">Janet Sudoku - project web page</a><br/>
+	/// <a href="http://github.com/mariuszgromada/Janet-Sudoku" target="_blank">Janet Sudoku on GitHub</a><br/>
+	/// <a href="http://janetsudoku.codeplex.com" target="_blank">Janet Sudoku on CodePlex</a><br/>
+	/// <a href="http://sourceforge.net/projects/janetsudoku" target="_blank">Janet Sudoku on SourceForge</a><br/>
+	/// <a href="http://bitbucket.org/mariuszgromada/janet-sudoku" target="_blank">Janet Sudoku on BitBucket</a><br/>
+	/// <a href="https://play.google.com/store/apps/details?id=org.mathparser.scalar.lite" target="_blank">Scalar Free</a><br/>
+	/// <a href="https://play.google.com/store/apps/details?id=org.mathparser.scalar.pro" target="_blank">Scalar Pro</a><br/>
+	/// <a href="http://scalarmath.org/" target="_blank">ScalarMath.org</a>
+	/// <para/>
+	/// Version: 4.0.0
+	/// </remarks>
 	[CLSCompliant(true)]
 	public sealed class AstronomicalConstants {
-		/**
-		 * Light year
-		 */
+		/// <summary>Light year</summary>
 		public const double LIGHT_YEAR = PhysicalConstants.LIGHT_SPEED * Units.JULIAN_YEAR;
-		/**
-		 * Astronomical unit
-		 */
+		/// <summary>Astronomical unit</summary>
 		public const double ASTRONOMICAL_UNIT = 149597870700.0 * Units.METRE;
-		/**
-		 * Parsec
-		 */
+		/// <summary>Parsec</summary>
 		public const double PARSEC = 206264.806247096 * ASTRONOMICAL_UNIT;
-		/**
-		 * Kiloparsec
-		 */
+		/// <summary>Kiloparsec</summary>
 		public const double KILOPARSEC = Units.KILO * PARSEC;
-		/**
-		 * Earth equatorial radius
-		 */
+		/// <summary>Earth equatorial radius</summary>
 		public const double EARTH_RADIUS_EQUATORIAL = 6378.1370 * Units.KILOMETRE;
-		/**
-		 * Earth polar radius
-		 */
+		/// <summary>Earth polar radius</summary>
 		public const double EARTH_RADIUS_POLAR = 6356.7523 * Units.KILOMETRE;
-		/**
-		 * Earth mean radius
-		 */
+		/// <summary>Earth mean radius</summary>
 		public const double EARTH_RADIUS_MEAN = 6371.0088 * Units.KILOMETRE;
-		/**
-		 * Earth mass
-		 */
+		/// <summary>Earth mass</summary>
 		public const double EARTH_MASS = 5.9722 * Units.YOTTA * Units.KILOGRAM;
-		/**
-		 * Earth semi-major axis
-		 */
+		/// <summary>Earth semi-major axis</summary>
 		public const double EARTH_SEMI_MAJOR_AXIS = 1.000001018 * ASTRONOMICAL_UNIT;
-		/**
-		 * Moon mean radius
-		 */
+		/// <summary>Moon mean radius</summary>
 		public const double MOON_RADIUS_MEAN = 1737.1 * Units.KILOMETRE;
-		/**
-		 * Moon mass
-		 */
+		/// <summary>Moon mass</summary>
 		public const double MOON_MASS = 0.012300037 * EARTH_MASS;
-		/**
-		 * Moon semi-major axis
-		 */
+		/// <summary>Moon semi-major axis</summary>
 		public const double MONN_SEMI_MAJOR_AXIS = 384399 * Units.KILOMETRE;
-		/**
-		 * Solar radius
-		 */
+		/// <summary>Solar radius</summary>
 		public const double SOLAR_RADIUS = 695700 * Units.KILOMETRE;
-		/**
-		 * Solar mass
-		 */
+		/// <summary>Solar mass</summary>
 		public const double SOLAR_MASS = 332946.0487 * EARTH_MASS;
-		/**
-		 * Mercury radius
-		 */
+		/// <summary>Mercury radius</summary>
 		public const double MERCURY_RADIUS_MEAN = 2439.7 * Units.KILOMETRE;
-		/**
-		 * Mercury mass
-		 */
+		/// <summary>Mercury mass</summary>
 		public const double MERCURY_MASS = 0.0553 * EARTH_MASS;
-		/**
-		 * Mercury semi-major axis
-		 */
+		/// <summary>Mercury semi-major axis</summary>
 		public const double MERCURY_SEMI_MAJOR_AXIS = 0.387098 * ASTRONOMICAL_UNIT;
-		/**
-		 * Venus radius
-		 */
+		/// <summary>Venus radius</summary>
 		public const double VENUS_RADIUS_MEAN = 6051.8 * Units.KILOMETRE;
-		/**
-		 * Venus mass
-		 */
+		/// <summary>Venus mass</summary>
 		public const double VENUS_MASS = 0.815 * EARTH_MASS;
-		/**
-		 * Venus semi-major axis
-		 */
+		/// <summary>Venus semi-major axis</summary>
 		public const double VENUS_SEMI_MAJOR_AXIS = 0.723332 * ASTRONOMICAL_UNIT;
-		/**
-		 * Mars radius
-		 */
+		/// <summary>Mars radius</summary>
 		public const double MARS_RADIUS_MEAN = 3389.5 * Units.KILOMETRE;
-		/**
-		 * Mars mass
-		 */
+		/// <summary>Mars mass</summary>
 		public const double MARS_MASS = 0.107 * EARTH_MASS;
-		/**
-		 * Mars semi-major axis
-		 */
+		/// <summary>Mars semi-major axis</summary>
 		public const double MARS_SEMI_MAJOR_AXIS = 1.523679 * ASTRONOMICAL_UNIT;
-		/**
-		 * Jupiter radius
-		 */
+		/// <summary>Jupiter radius</summary>
 		public const double JUPITER_RADIUS_MEAN = 69911 * Units.KILOMETRE;
-		/**
-		 * Jupiter mass
-		 */
+		/// <summary>Jupiter mass</summary>
 		public const double JUPITER_MASS = 317.8 * EARTH_MASS;
-		/**
-		 * Jupiter semi-major axis
-		 */
+		/// <summary>Jupiter semi-major axis</summary>
 		public const double JUPITER_SEMI_MAJOR_AXIS = 5.20260 * ASTRONOMICAL_UNIT;
-		/**
-		 * Saturn radius
-		 */
+		/// <summary>Saturn radius</summary>
 		public const double SATURN_RADIUS_MEAN = 58232 * Units.KILOMETRE;
-		/**
-		 * Saturn mass
-		 */
+		/// <summary>Saturn mass</summary>
 		public const double SATURN_MASS = 95.159 * EARTH_MASS;
-		/**
-		 * Saturn semi-major axis
-		 */
+		/// <summary>Saturn semi-major axis</summary>
 		public const double SATURN_SEMI_MAJOR_AXIS = 9.5549 * ASTRONOMICAL_UNIT;
-		/**
-		 * Uranus radius
-		 */
+		/// <summary>Uranus radius</summary>
 		public const double URANUS_RADIUS_MEAN = 25362 * Units.KILOMETRE;
-		/**
-		 * Uranus mass
-		 */
+		/// <summary>Uranus mass</summary>
 		public const double URANUS_MASS = 14.536 * EARTH_MASS;
-		/**
-		 * Uranus semi-major axis
-		 */
+		/// <summary>Uranus semi-major axis</summary>
 		public const double URANUS_SEMI_MAJOR_AXIS = 19.2184 * ASTRONOMICAL_UNIT;
-		/**
-		 * Neptune radius
-		 */
+		/// <summary>Neptune radius</summary>
 		public const double NEPTUNE_RADIUS_MEAN = 24622 * Units.KILOMETRE;
-		/**
-		 * Neptune mass
-		 */
+		/// <summary>Neptune mass</summary>
 		public const double NEPTUNE_MASS = 17.147 * EARTH_MASS;
-		/**
-		 * Neptune semi-major axis
-		 */
+		/// <summary>Neptune semi-major axis</summary>
 		public const double NEPTUNE_SEMI_MAJOR_AXIS = 30.110387 * ASTRONOMICAL_UNIT;
 	}
 }
