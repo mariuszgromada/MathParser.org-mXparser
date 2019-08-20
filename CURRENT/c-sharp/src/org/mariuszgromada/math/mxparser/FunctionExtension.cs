d23e042dd3b79d4701b36922bd2335b5f5b3af3b
Adam Spofford <pieflavor.mc@gmail.com>
2019-08-19 17:16:49 -0700

    Document FunctionExtension.cs

--- CURRENT/c-sharp/src/org/mariuszgromada/math/mxparser/FunctionExtension.cs
+++ CURRENT/c-sharp/src/org/mariuszgromada/math/mxparser/FunctionExtension.cs
@@ -56,76 +56,69 @@
 using System;
 
 namespace org.mariuszgromada.math.mxparser {
-	/**
-	 * FunctionExtension provides interface for function algorithm definition.
-	 * In this case algorithm definition is based on source code using
-	 * JAVA (for JAVA / Android) or .NET. If implemented Function Extension
-	 * object can be further used while Function object
-	 * construction, which means it can extend mXparser math collection.
-	 * mXparser extension with your own implementation can be achieved
-	 * by implementing FunctionExtension interface, creating an FunctionExtension
-	 * object, creating Function object based on FunctionExtension, adding Function
-	 * object to Expression / mXparser definition.
-	 *
-	 * @author         <b>Mariusz Gromada</b><br>
-	 *                 <a href="mailto:mariuszgromada.org@gmail.com">mariuszgromada.org@gmail.com</a><br>
-	 *                 <a href="http://mathspace.pl" target="_blank">MathSpace.pl</a><br>
-	 *                 <a href="http://mathparser.org" target="_blank">MathParser.org - mXparser project page</a><br>
-	 *                 <a href="http://github.com/mariuszgromada/MathParser.org-mXparser" target="_blank">mXparser on GitHub</a><br>
-	 *                 <a href="http://mxparser.sourceforge.net" target="_blank">mXparser on SourceForge</a><br>
-	 *                 <a href="http://bitbucket.org/mariuszgromada/mxparser" target="_blank">mXparser on Bitbucket</a><br>
-	 *                 <a href="http://mxparser.codeplex.com" target="_blank">mXparser on CodePlex</a><br>
-	 *                 <a href="http://janetsudoku.mariuszgromada.org" target="_blank">Janet Sudoku - project web page</a><br>
-	 *                 <a href="http://github.com/mariuszgromada/Janet-Sudoku" target="_blank">Janet Sudoku on GitHub</a><br>
-	 *                 <a href="http://janetsudoku.codeplex.com" target="_blank">Janet Sudoku on CodePlex</a><br>
-	 *                 <a href="http://sourceforge.net/projects/janetsudoku" target="_blank">Janet Sudoku on SourceForge</a><br>
-	 *                 <a href="http://bitbucket.org/mariuszgromada/janet-sudoku" target="_blank">Janet Sudoku on BitBucket</a><br>
-	 *                 <a href="https://play.google.com/store/apps/details?id=org.mathparser.scalar.lite" target="_blank">Scalar Free</a><br>
-	 *                 <a href="https://play.google.com/store/apps/details?id=org.mathparser.scalar.pro" target="_blank">Scalar Pro</a><br>
-	 *                 <a href="http://scalarmath.org/" target="_blank">ScalarMath.org</a><br>
-	 *
-	 * @version        4.1.0
-	 *
-	 * @see Function
-	 *
-	 */
+	/// <summary>
+	/// FunctionExtension provides interface for function algorithm definition.
+	/// In this case algorithm definition is based on source code using
+	/// JAVA (for JAVA / Android) or .NET. If implemented Function Extension
+	/// object can be further used while <see cref="Function"/> object
+	/// construction, which means it can extend <see cref="mXparser"/> math collection.
+	/// mXparser extension with your own implementation can be achieved
+	/// by implementing FunctionExtension interface, creating an FunctionExtension
+	/// object, creating <see cref="Function"/> object based on FunctionExtension, adding <see cref="Function"/>
+	/// object to <see cref="Expression"/> / <see cref="mXparser"/> definition.
+	/// </summary>
+	///
+	/// <remarks>
+	/// Author: <b>Mariusz Gromada</b><br/>
+	/// <a href="mailto:mariuszgromada.org@gmail.com">mariuszgromada.org@gmail.com</a><br/>
+	/// <a href="http://mathspace.pl" target="_blank">MathSpace.pl</a><br/>
+	/// <a href="http://mathparser.org" target="_blank">MathParser.org - mXparser project page</a><br/>
+	/// <a href="http://github.com/mariuszgromada/MathParser.org-mXparser" target="_blank">mXparser on GitHub</a><br/>
+	/// <a href="http://mxparser.sourceforge.net" target="_blank">mXparser on SourceForge</a><br/>
+	/// <a href="http://bitbucket.org/mariuszgromada/mxparser" target="_blank">mXparser on Bitbucket</a><br/>
+	/// <a href="http://mxparser.codeplex.com" target="_blank">mXparser on CodePlex</a><br/>
+	/// <a href="http://janetsudoku.mariuszgromada.org" target="_blank">Janet Sudoku - project web page</a><br/>
+	/// <a href="http://github.com/mariuszgromada/Janet-Sudoku" target="_blank">Janet Sudoku on GitHub</a><br/>
+	/// <a href="http://janetsudoku.codeplex.com" target="_blank">Janet Sudoku on CodePlex</a><br/>
+	/// <a href="http://sourceforge.net/projects/janetsudoku" target="_blank">Janet Sudoku on SourceForge</a><br/>
+	/// <a href="http://bitbucket.org/mariuszgromada/janet-sudoku" target="_blank">Janet Sudoku on BitBucket</a><br/>
+	/// <a href="https://play.google.com/store/apps/details?id=org.mathparser.scalar.lite" target="_blank">Scalar Free</a><br/>
+	/// <a href="https://play.google.com/store/apps/details?id=org.mathparser.scalar.pro" target="_blank">Scalar Pro</a><br/>
+	/// <a href="http://scalarmath.org/" target="_blank">ScalarMath.org</a>
+	/// <para/>
+	/// Version: 4.1.0
+	/// </remarks>
+	/// 
+	/// <seealso cref="Function"/>
 	[CLSCompliant(true)]
 	public interface FunctionExtension {
-		/**
-		 * Gets parameters number.
-		 *
-		 * @return Returns parameters number.
-		 */
+		/// <summary>Gets parameters number.</summary>
+		///
+		/// <returns>Returns parameters number.</returns>
 		int getParametersNumber();
-		/**
-		 * Sets value of function parameter
-		 *
-		 * @param parameterIndex    - parameter index (from 0 to n-1)
-		 * @param parameterValue    - parameter value
-		 */
+		/// <summary>Sets value of function parameter</summary>
+		///
+		/// <param name="parameterIndex">parameter index (from 0 to n-1)</param>
+		/// <param name="parameterValue">parameter value</param>
 		void setParameterValue(int parameterIndex, double parameterValue);
-		/**
-		 * Gets parameter name
-		 *
-		 * @param parameterIndex    - parameter index (from 0 to n-1)
-		 * @return Returns parameter name
-		 */
+		/// <summary>Gets parameter name</summary>
+		///
+		/// <param name="parameterIndex">parameter index (from 0 to n-1)</param>
+		/// 
+		/// <returns>Returns parameter name</returns>
 		String getParameterName(int parameterIndex);
-		/**
-		 * Actual algorithm implementation.
-		 *
-		 * @param params Function parameters.
-		 * @return Function Extension value.
-		 */
+		/// <summary>Actual algorithm implementation</summary>
+		/// 
+		/// <returns>Function Extension value.</returns>
 		double calculate();
-		/**
-		 * Cloning in case of usage in Expression
-		 * with recursive statements.
-		 *
-		 * @return Returns FunctionExtension object that was cloned.
-		 *
-		 * @see Expression#getRecursiveMode()
-		 */
+		/// <summary>
+		/// Cloning in case of usage in Expression
+		/// with recursive statements.
+		/// </summary>
+		///
+		/// <returns>Returns FunctionExtension object that was cloned.</returns>
+		///
+		/// <seealso cref="Expression.getRecursiveMode()"/>
 		FunctionExtension clone();
 	}
 }
