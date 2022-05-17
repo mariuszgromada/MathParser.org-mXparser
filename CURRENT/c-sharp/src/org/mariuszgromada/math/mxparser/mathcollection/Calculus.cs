8a2dfc8e649be368a7e00ec433c9a2623c965d2b
Mariusz Gromada <mariusz.gromada@gmail.com>
2022-05-18 00:22:09 +0200

    5.0.3 file headers

--- CURRENT/c-sharp/src/org/mariuszgromada/math/mxparser/mathcollection/Calculus.cs
+++ CURRENT/c-sharp/src/org/mariuszgromada/math/mxparser/mathcollection/Calculus.cs
@@ -1,19 +1,21 @@
 /*
- * @(#)Calculus.cs        5.0.0    2022-04-10
+ * @(#)Calculus.cs        5.0.3    2022-05-17
  *
- * Copyright 2010 - 2022 MARIUSZ GROMADA. All rights reserved.
+ * MathParser.org-mXparser DUAL LICENSE AGREEMENT
  *
- * PRODUCT: MathParser.org-mXparser
- * LICENSE: DUAL LICENSE
+ * AUTHOR: Copyright 2010 - 2022 Mariusz Gromada - All rights reserved
+ * PUBLISHER: INFIMA - https://payhip.com/infima
  *
  * SOFTWARE means source code and/or binary form and/or documentation.
+ * PRODUCT: MathParser.org-mXparser SOFTWARE
+ * LICENSE: DUAL LICENSE AGREEMENT
  *
- * BY INSTALLING, COPYING, OR OTHERWISE USING THE SOFTWARE, YOU AGREE TO BE
+ * BY INSTALLING, COPYING, OR OTHERWISE USING THE PRODUCT, YOU AGREE TO BE
  * BOUND BY ALL OF THE TERMS AND CONDITIONS OF THE DUAL LICENSE AGREEMENT.
  *
- * MARIUSZ GROMADA provides MathParser.org-mXparser SOFTWARE under the
- * DUAL LICENSE model designed to meet the needs of both Non-Commercial Use
- * as well as Commercial Use.
+ * AUTHOR & PUBLISHER provide the PRODUCT under the DUAL LICENSE AGREEMENT
+ * model designed to meet the needs of both non-commercial use as well as
+ * commercial use.
  *
  * NON-COMMERCIAL USE means any use or activity where a fee is not charged
  * and the purpose is not the sale of a good or service, and the use or
@@ -32,81 +34,131 @@
  * 4. Other distributors that combine and distribute commercially licensed
  *    software.
  *
- * IN CASE YOU WANT TO USE THE SOFTWARE COMMERCIALLY, YOU MUST PURCHASE
- * THE APPROPRIATE LICENSE FROM "INFIMA IWONA GLOWACKA-GROMADA", ONLINE
- * STORE ADDRESS: HTTPS://PAYHIP.COM/INFIMA
+ * IN CASE YOU WANT TO USE THE PRODUCT COMMERCIALLY, YOU MUST PURCHASE THE
+ * APPROPRIATE LICENSE FROM "INFIMA" ONLINE STORE, STORE ADDRESS:
+ *
+ * 1. https://mathparser.org/order-commercial-license
+ * 2. https://payhip.com/infima
  *
  * NON-COMMERCIAL LICENSE
  *
- * Redistribution and use of the PRODUCT in source and/or binary forms, with
- * or without modification, are permitted provided that the following
+ * Redistribution and use of the PRODUCT in source and/or binary forms,
+ * with or without modification, are permitted provided that the following
  * conditions are met:
  *
  * 1. Redistributions of source code must retain unmodified content of the
- *    entire MathParser.org-mXparser DUAL LICENSE, including definition of
- *    NON-COMMERCIAL USE, definition of COMMERCIAL USE, NON-COMMERCIAL
- *    LICENSE conditions, COMMERCIAL LICENSE conditions, and the following
- *    DISCLAIMER.
+ *    entire MathParser.org-mXparser DUAL LICENSE AGREEMENT, including
+ *    definition of NON-COMMERCIAL USE, definition of COMMERCIAL USE,
+ *    NON-COMMERCIAL LICENSE conditions, COMMERCIAL LICENSE conditions, and
+ *    the following DISCLAIMER.
  * 2. Redistributions in binary form must reproduce the entire content of
- *    MathParser.org-mXparser DUAL LICENSE in the documentation and/or other
- *    materials provided with the distribution, including definition of
- *    NON-COMMERCIAL USE, definition of COMMERCIAL USE, NON-COMMERCIAL
- *    LICENSE conditions, COMMERCIAL LICENSE conditions, and the following
- *    DISCLAIMER.
+ *    MathParser.org-mXparser DUAL LICENSE AGREEMENT in the documentation
+ *    and/or other materials provided with the distribution, including
+ *    definition of NON-COMMERCIAL USE, definition of COMMERCIAL USE,
+ *    NON-COMMERCIAL LICENSE conditions, COMMERCIAL LICENSE conditions, and
+ *    the following DISCLAIMER.
  *
  * COMMERCIAL LICENSE
  *
- *  1. Before purchasing a commercial license, MARIUSZ GROMADA allows you to
- *     download, install and use up to three copies of the PRODUCT to perform
- *     integration tests, confirm the quality of the PRODUCT and its
- *     suitability. The testing period should be limited to one month. Tests
- *     should take place via test environments. The purpose of the tests must
- *     not be to generate profit.
- *  2. Provided that you purchased a license from "INFIMA IWONA GLOWACKA-GROMADA"
- *     (online store address: https://payhip.com/INFIMA), you comply with all
- *     below terms and conditions, and you have acknowledged and understood the
- *     following DISCLAIMER, MARIUSZ GROMADA grants you a nonexclusive license
+ *  1. Before purchasing a commercial license, AUTHOR & PUBLISHER allow you
+ *     to download, install and use up to three copies of the PRODUCT to
+ *     perform integration tests, confirm the quality of the PRODUCT and
+ *     its suitability. The testing period should be limited to fourteen
+ *     days. Tests should be performed under the conditions of test
+ *     environments. The purpose of the tests must not be to generate profit.
+ *  2. Provided that you purchased a license from "INFIMA" online store
+ *     (store address: https://mathparser.org/order-commercial-license or
+ *     https://payhip.com/infima), and you comply with all below terms and
+ *     conditions, and you have acknowledged and understood the following
+ *     DISCLAIMER, AUTHOR & PUBLISHER grant you a nonexclusive license
  *     including the following rights:
- *  3. In case you purchased a "Single License" You can install and use the
+ *  3. The license has been granted only to you, i.e., the person or entity
+ *     that made the purchase, who is identified and confirmed by the data
+ *     provided during the purchase.
+ *  4. In case you purchased a license in the "ONE-TIME PURCHASE" model,
+ *     the license has been granted only for the PRODUCT version specified
+ *     in the purchase. The upgrade policy gives you additional rights and
+ *     is described in the dedicated section below.
+ *  5. In case you purchased a license in the "SUBSCRIPTION" model, you can
+ *     install and use any version of the PRODUCT, but only during the
+ *     subscription validity period.
+ *  6. In case you purchased a "SINGLE LICENSE" you can install and use the
  *     PRODUCT from one workstation.
- *  4. Additional copies of the PRODUCT can be installed and used from more
- *     than one workstation; however, this number is limited to the number of
- *     copies purchased as per order.
- *  5. In case you purchased a "Site License", the PRODUCT can be installed
+ *  7. Additional copies of the PRODUCT can be installed and used from more
+ *     than one workstation; however, this number is limited to the number
+ *     of workstations purchased as per order.
+ *  8. In case you purchased a "SITE LICENSE ", the PRODUCT can be installed
  *     and used from all workstations located at your premises.
- *  6. You may incorporate the unmodified PRODUCT into your own products and
- *     software.
- *  7. If you purchased a license with the "Source Code" option, you may modify
- *     the PRODUCT's source code and incorporate the modified source code into
- *     your own products and / or software.
- *  8. You may distribute your product and / or software with the incorporated
+ *  9. You may incorporate the unmodified PRODUCT into your own products
+ *     and software.
+ * 10. If you purchased a license with the "SOURCE CODE" option, you may
+ *     modify the PRODUCT's source code and incorporate the modified source
+ *     code into your own products and/or software.
+ * 11. Provided that the license validity period has not expired, you may
+ *     distribute your product and/or software with the incorporated
  *     PRODUCT royalty-free.
- *  9. You may make copies of the PRODUCT for backup and archival purposes.
- * 10. MARIUSZ GROMADA reserves all rights not expressly granted to you in
- *     this agreement.
+ * 12. You may make copies of the PRODUCT for backup and archival purposes.
+ * 13. AUTHOR & PUBLISHER reserve all rights not expressly granted to you
+ *     in this agreement.
  *
- * CONTACT
- * - e-mail: info@mathparser.org
- * - website: https://mathparser.org
- * - source code: https://github.com/mariuszgromada/MathParser.org-mXparser
- * - online store: https://payhip.com/INFIMA
+ * ADDITIONAL CLARIFICATION ON WORKSTATION
+ *
+ * A workstation is a device, a remote device, or a virtual device, used by
+ * you, your employees, or other entities to whom you have commissioned the
+ * tasks. For example, the number of workstations may refer to the number
+ * of software developers, engineers, architects, scientists, and other
+ * professionals who use the PRODUCT on your behalf. The number of
+ * workstations is not the number of copies of your end-product that you
+ * distribute to your end-users.
+ *
+ * By purchasing the COMMERCIAL LICENSE, you only pay for the number of
+ * workstations, while the number of copies of your final product
+ * (delivered to your end-users) is not limited.
+ *
+ * UPGRADE POLICY
+ *
+ * The PRODUCT is versioned according to the following convention:
+ *
+ *    [MAJOR].[MINOR].[PATCH]
+ *
+ * 1. COMMERCIAL LICENSE holders can install and use the updated version
+ *    for bug fixes free of charge, i.e. if you have purchased a license
+ *    for the [MAJOR].[MINOR] version (e.g.: 5.0), you can freely install
+ *    all the various releases specified in the [PATCH] version (e.g.: 5.0.2).
+ *    The license terms remain unchanged after the update.
+ * 2. COMMERCIAL LICENSE holders for [MAJOR].[MINOR] version (e.g.: 5.0)
+ *    can install and use the updated version [MAJOR].[MINOR + 1] free of
+ *    charge, i.e., plus one release in the [MINOR] range (e.g.: 5.1). The
+ *    license terms remain unchanged after the update.
+ * 3. COMMERCIAL LICENSE holders who wish to upgrade their version, but are
+ *    not eligible for the free upgrade, can claim a discount when
+ *    purchasing the upgrade. For this purpose, please contact us via e-mail.
  *
  * DISCLAIMER
  *
- * THIS SOFTWARE IS PROVIDED BY MARIUSZ GROMADA "AS IS" AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL MATHPARSER.ORG MARIUSZ GROMADA OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
- * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS PRODUCT IS PROVIDED BY AUTHOR & PUBLISHER "AS IS" AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL AUTHOR OR PUBLISHER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS PRODUCT, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
  *
- * THE VIEWS AND CONCLUSIONS CONTAINED IN THE SOFTWARE AND DOCUMENTATION ARE
- * THOSE OF THE AUTHORS AND SHOULD NOT BE INTERPRETED AS REPRESENTING OFFICIAL
- * POLICIES, EITHER EXPRESSED OR IMPLIED, OF MARIUSZ GROMADA.
+ * THE VIEWS AND CONCLUSIONS CONTAINED IN THE PRODUCT AND DOCUMENTATION ARE
+ * THOSE OF THE AUTHORS AND SHOULD NOT BE INTERPRETED AS REPRESENTING
+ * OFFICIAL POLICIES, EITHER EXPRESSED OR IMPLIED, OF AUTHOR OR PUBLISHER.
+ *
+ * CONTACT
+ *
+ * - e-mail: info@mathparser.org
+ * - website: https://mathparser.org
+ * - source code: https://github.com/mariuszgromada/MathParser.org-mXparser
+ * - online store: https://mathparser.org/order-commercial-license
+ * - online store: https://payhip.com/infima
  */
 using System;
 
