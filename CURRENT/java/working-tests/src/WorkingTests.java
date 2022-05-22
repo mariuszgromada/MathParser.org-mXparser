514f2742a87ebe0e8d1b13b472cec24d41496789
Mariusz Gromada <mariusz.gromada@gmail.com>
2022-05-22 16:56:21 +0200

    Update WorkingTests.java

--- CURRENT/java/working-tests/src/WorkingTests.java
+++ CURRENT/java/working-tests/src/WorkingTests.java
@@ -1,23 +1,11 @@
 import org.mariuszgromada.math.mxparser.*;
-import org.mariuszgromada.math.mxparser.mathcollection.MathConstants;
-import org.mariuszgromada.math.mxparser.mathcollection.NumberTheory;
-import org.mariuszgromada.math.mxparser.mathcollection.ProbabilityDistributions;
-import org.mariuszgromada.math.mxparser.mathcollection.SpecialFunctions;
-import org.mariuszgromada.math.mxparser.parsertokens.KeyWord;
-import org.mariuszgromada.math.mxparser.parsertokens.Token;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.io.*;
 
 public class WorkingTests {
 
-	public static String stringRepeat(String s, int n) {
-		String o = "";
-		for (int i = 0; i < n; i++) { o = o + s; }
-		return o;
-	}
 	public static void main(String[] args) {
-		mXparser.consolePrintLicense();
+		Argument x = new Argument("x = 10");
+		Expression e = new Expression("1+2*x", x);
+		mXparser.consolePrintln(e.getExpressionString() + " = " + e.calculate());
 	}
+
 }
