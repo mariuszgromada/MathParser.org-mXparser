ab051ca105b8cbb3f82651d96a7f7efc787171dc
Mariusz Gromada <mariusz.gromada@gmail.com>
2024-05-19 22:28:27 +0200

    v.6.0 doc

--- CURRENT/doc/package-search-index.js
+++ CURRENT/doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.mariuszgromada.math.mxparser"},{"l":"org.mariuszgromada.math.mxparser.mathcollection"},{"l":"org.mariuszgromada.math.mxparser.parsertokens"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.mariuszgromada.math.mxparser"},{"l":"org.mariuszgromada.math.mxparser.mathcollection"},{"l":"org.mariuszgromada.math.mxparser.parsertokens"}];updateSearchResults();
